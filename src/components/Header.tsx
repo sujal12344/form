974114b52a4d4aad6e27152b09f882e382043b88
sujal <sujalkesharwani220@gmail.com>
2025-02-12 02:32:59 +0530

    fixes prod issue

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,14 @@
+import Image from "next/image";
+
 const Header = () => {
   return (
     <header className="bg-[#142239] p-4 flex items-center gap-2">
-      <img 
+      <Image 
         src="/logo.bmp" 
         alt="DES SOLAR PRIVATE LIMITED" 
         // width={60}
         // height={80}
-        className="w-[40px] h-[60px] sm:w-[60px] sm:h-[80px]"
+        className="w-[40px] h-[40px] sm:w-[60px] sm:h-[60px]"
       />
       <h1 className="text-white text-[13px] sm:text-base lg:text-lg font-semibold">DRS SOLAR PRIVATE LIMITED</h1>
     </header>
